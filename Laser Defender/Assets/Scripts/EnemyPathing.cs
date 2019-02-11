8936225c0ac971ffdaf9961512d93429c6713bd5
Aliana Crusis <47539483+alianacrusis@users.noreply.github.com>
2019-02-11 15:35:06 -0500

    Initial Project File

---
+++ Laser Defender/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+
+    [SerializeField] List<Transform> waypoints = default;
+    [SerializeField] float moveSpeed = default;
+
+    int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+   
+        if (waypointIndex < waypoints.Count)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if ((transform.position.x == targetPosition.x) && (transform.position.y == targetPosition.y))
+            {
+                waypointIndex++;
+            }
+
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
